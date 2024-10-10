1a70fd39ef89182c8fbaa5e8a09dc1ff6dd78a22
Robariza <robinsonariza2001@gmail.com>
2024-10-09 20:12:49 -0500

    [update] admin services responsive

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuNavComponent } from './components/menu-nav/menu-nav.component';
 import { FooterComponent } from './components/footer/footer.component';
